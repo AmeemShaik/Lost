f6685397ffa92e716ec8010971340be529d5badc
Ameem Shaik <ameem91@gmail.com>
2015-02-28 02:04:36 -0500

    Added two text fields and a button. Next, I need to make it actually do something

--- app/src/main/java/com/example/hackathon/lost/MainActivity.java
+++ app/src/main/java/com/example/hackathon/lost/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +36,9 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void getDirections(View button){
+        System.out.println("Button is clicked!!");
     }
 }
