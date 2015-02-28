b954bbcbc19b3703e762b4c5ebbdeccab8dce499
Azim Shaik <azimshaik91@gmail.com>
2015-02-28 16:27:23 -0600

    Scrolling and Directions Done :)

--- app/src/main/java/com/example/hackathon/lost/MainActivity.java
+++ app/src/main/java/com/example/hackathon/lost/MainActivity.java
@@ -1,22 +1,42 @@
 package com.example.hackathon.lost;
 
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
     public static final String API_KEY="AIzaSyA8F60HygLJWrkeezbTvj392tDuf7s1aW0";
+
+    private TextView text2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
 
@@ -50,6 +70,75 @@ public class MainActivity extends ActionBarActivity {
         String URL = String.format("https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&key=%s",origin, destination, API_KEY);
 
         System.out.println(URL);
+
         new GetDirectionsTask().execute(URL);
+
+
+    }
+    public class GetDirectionsTask extends AsyncTask<String, Void, String> {
+
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            String URL = params[0];
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try {
+                response = httpClient.execute(new HttpGet(URL));
+                StatusLine statusLine = response.getStatusLine();
+                if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    responseString = out.toString();
+                    out.close();
+                } else {
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return responseString;
+        }
+
+
+
+
+        @Override
+        protected void onPostExecute(String data) {
+            super.onPostExecute(data);
+
+
+            try {
+                JSONObject jsonData = new JSONObject(data);
+                JSONArray jsonSteps = jsonData.getJSONArray("routes")
+                        .getJSONObject(0).getJSONArray("legs")
+                        .getJSONObject(0).getJSONArray("steps");
+
+                JSONObject jsonCurrent = null;
+                String current = null;
+                LinearLayout directionList = (LinearLayout) findViewById(R.id.main_layout);
+
+                for(int i = 0; i < jsonSteps.length(); i++){
+                    jsonCurrent = jsonSteps.getJSONObject(i);
+                    current = jsonCurrent.getString("html_instructions").replaceAll("\\<.*?\\>", "");
+                    System.out.println(current);
+                    TextView currentView = new TextView(getApplicationContext());
+                    currentView.setText(current);
+                    currentView.setTextColor(Color.BLACK);
+                    directionList.addView(currentView);
+                }
+//                TextView textView = (TextView) findViewById(R.id.textView);
+//                textView.setText(current);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            //text2 = (TextView) findViewById(R.id.text2);
+
+        }
     }
 }
